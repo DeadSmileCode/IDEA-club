3c2fb0b2cd4220d88b614c472196797e04703227
DeadSmile <sachev.andrei@gmail.com>
2022-08-31 01:11:11 +0600

    Update style and create new page "About"

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ const file_games = './html/games_statistic.json';
 
 site.use(express.static(__dirname + "/www"));
 
+site.get("/" , function(req ,res) {
+	res.sendFile("./www/index.html" , { root: '.' });
+});
+site.get("/about" , function(req ,res) {
+	res.sendFile("./www/about.html" , { root: '.' });
+});
+site.get("/progress" , function(req ,res) {
+	res.sendFile("./www/index.html" , { root: '.' });
+});
+site.get("/about/system" , function(req ,res) {
+	res.sendFile("./www/about_system.html" , { root: '.' });
+});
+site.get("/about/departments" , function(req ,res) {
+	res.sendFile("./www/deportaments.html" , { root: '.' });
+});
+site.get("/how-link" , function(req ,res) {
+	res.sendFile("./www/how-link.html" , { root: '.' });
+});
 
 site.listen(process.env.PORT);
 
